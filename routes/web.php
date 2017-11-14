d759e9ff4fe5c2069a8b20dcde5f5e859141a2f0
rizkimaida <rizkimaida@gmail.com>
2017-11-14 08:58:52 +0700

    menambahkan laravel dari web lanjut

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,46 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('pesan', function () {
+    return view('pesan');
+});
+
+Route::post('pesan/kirim', function () {
+    return view('pesan_kirim');
+});
+
+
+Route::get('template', function () {
+    return view('coba1');
+});
+
+Route::get('coba1', function () {
+    return view('coba1');
+});
+
+Route::get('coba2', function () {
+    return view('coba2');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::post('/users/logout', 'Auth\LoginController@userLogout')->name('user.logout');
+
+Route::prefix('admin')->group(function() {
+  Route::get('/', 'AdminController@index')->name('admin.dashboard');
+  Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+  Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+  Route::post('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+
+  // Password reset routes
+  Route::post('/password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+  Route::get('/password/reset', 'Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+  Route::post('/password/reset', 'Auth\AdminResetPasswordController@reset');
+  Route::get('/password/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
+});
